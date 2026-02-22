5e4f1bee1be4384e0267fc2f72a551e58cb04373
ishan027a-dotcom <ishan.027a@gmail.com>
2026-02-23 02:40:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,11 +72,41 @@ async def on_message(message):
     if user.id in active_sessions:
 
         warn = await message.channel.send(
-            f"⚠️ {user.mention} You are already marked ONLINE"
-        )
+            f"⚠️ {user.mention} You are already marked ONLINE"await warn.delete(delay=3)
+        return)
+# ---------------- ONLINE ---------------- #
 
-        await warn.delete(delay=3)
-        return
+    if content == "online":
+
+        try:
+            await message.delete()
+        except:
+            pass
+
+        if user.id not in active_sessions:
+
+            active_sessions[user.id] = now
+
+            if is_leader:
+                desc = f"🛡️ Leader **{user.mention}** is watching."
+                color = 0xf1c40f
+            else:
+                desc = f"✅ **{user.mention}** has started their session."
+                color = 0x2ecc71
+
+            embed = discord.Embed(
+                title="Status: ONLINE",
+                description=desc,
+                color=color
+            )
+
+            embed.set_author(name=user.display_name, icon_url=user.display_avatar.url)
+            embed.set_thumbnail(url=user.display_avatar.url)
+            embed.add_field(name="Arrival", value=f"<t:{timestamp}:t>")
+
+            await message.channel.send(embed=embed)
+
+        
 
 # ---------------- OFFLINE ---------------- #
 
